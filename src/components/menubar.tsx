12e44b6348e5be2a96da8009380eb650c5966021
Robert Okoroafor <robooko@hotmail.com>
2025-05-03 17:10:38 +0100

    react

---
+++ src/components/menubar.tsx
@@ -0,0 +1,84 @@
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+import { ReactSVG } from "react-svg";
+import { Navigation } from '../js/menu';
+
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function MenuBar() {
+  return (
+    <Disclosure as="nav" className="w-full overflow-hidden ">
+      <div className="mx-auto p-10">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* Mobile menu button*/}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-100 hover:bg-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
+              <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
+            </DisclosureButton>
+          </div>
+          <div className="flex">
+            <div className="flex flex-shrink-0 items-center">
+              <a href='/' className='w-full flex ml-12 sm:ml-0'>
+              <img className='h-10 mr-3' src="/logo.svg"></img>
+              </a>
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {Navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? 'page' : undefined}
+                    className={classNames(
+                      item.current ? ' text-gray-100' : '',
+                      'rounded-md px-1 md:px-3 py-2 font-medium text-gray-200',
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="inline-flex gap-1 md:gap-5">
+            
+          <ReactSVG
+              className={`header-social1 opacity-0 h-8 w-8 translate-x-[800px] `}
+              src={`../icons/linkedin-in.svg`}
+            />
+            <ReactSVG
+                className={`header-social2 opacity-0 h-8 w-8 translate-x-[800px]`}
+                src={`../icons/youtube.svg`}
+              />
+          </div>
+        </div>
+      </div>
+
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          {Navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? 'page' : undefined}
+              className={classNames(
+                item.current ? 'text-gray-900 ' : 'text-gray-800',
+                'block rounded-md px-3 py-2 text-lx3 font-medium',
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  )
+}
---
+++ src/js/menu.ts
@@ -0,0 +1,8 @@
+
+export const Navigation = [
+  { name: 'HOME', href: '/', current: false },
+    { name: 'SERVICES', href: '#services', current: false },
+    { name: 'GALLERY', href: '#gallery', current: false },
+    { name: 'CONTACT', href: '#contact', current: false },
+    { name: 'CAREERS', href: '#careers', current: false },
+  ];
