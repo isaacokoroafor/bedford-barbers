9c7d96831fd703c72cb2e90be574f059a9c26889
Robert Okoroafor <robooko@hotmail.com>
2025-05-05 23:32:57 +0100

    header

--- src/js/menu.ts
+++ src/js/menu.ts
@@ -1,9 +1,9 @@
 
 export const Navigation = [
-  { name: 'HOME', href: '/', current: false },
-    { name: 'SERVICES', href: '/#services', current: false },
-    { name: 'GALLERY', href: '/#gallery', current: false },
-    { name: 'CONTACT', href: '/#contact', current: false },
-    { name: 'BLOG', href: '/blog', current: false },
-    { name: 'CAREERS', href: '/careers', current: false },
+  { name: 'HOME', href: '/', title: "Home", current: false },
+    { name: 'SERVICES', href: '/#services', title: "Our Services", current: false  },
+    { name: 'GALLERY', href: '/#gallery', title: "Gallery of our work", current: false  },
+    { name: 'CONTACT', href: '/#contact', title: "Contact Us", current: false  },
+    { name: 'BLOG', href: '/blog', title: "Blog", current: false  },
+    { name: 'CAREERS', href: '/careers', title: "Careers", current: false  },
   ];
