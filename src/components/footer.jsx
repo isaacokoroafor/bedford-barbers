ef901c999760ff450497544794d34a80effb6eab
Robert Okoroafor <robooko@hotmail.com>
2025-05-07 20:18:50 +0100

    footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,25 +1,26 @@
 import { ReactSVG } from "react-svg";
 import { Footer } from "../js/footerLinks";
 import { socialLinks } from "@/js/socialLinks";
-
+import { LogoIcon } from './svg/logo';
 import { BooksyIcon } from './svg/booksy';
 export default function FooterMenu() {
   return (
     <footer className="w-full overflow-hidden">
       <div className="mx-auto max-w-6xl px-6 py-8">
         <div className="flex flex-col flex-shrink-0 items-center justify-center">
-          <a className=" flex ">
-            <img className="h-40 mb-4" src="/logo.svg"></img>
+          <a className=" flex mb-4 stroke-amber-400" href="/">
+            <LogoIcon ></LogoIcon>
           </a>
-          <p className="text-white">20C Miller Rd, Bedford MK42 9NZ</p>
+          <p className="text-white text-center">20C Miller Rd, Bedford MK42 9NZ</p>
         </div>
 
-        <div className="flex justify-center space-x-12 mt-8">
+        <div className="flex justify-center flex-wrap gap-x-12 mt-8">
           {Footer.map((item) => (
             <a
               key={item.name}
-              href={item.href}
-              className="text-white hover:text-green-500 transition-colors"
+              href={item.href} 
+              name={item.name}
+              className="text-white scale-95 hover:scale-100"
               aria-current={item.current ? "page" : undefined}
             >
               {item.name}
@@ -32,6 +33,7 @@ export default function FooterMenu() {
             <a
               key={name}
               href={href}
+              name={name}
               className="text-white"
               aria-current={name}
             >
